ca6ab8209ce247e45d6d8c35a165ac749d33b411
ThatPepLab <me@thateyeplace.com>
2026-08-04 13:23:59 -0500

    Add password gate and remove offline access

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
-const CACHE="peptide-price-comparison-august-v2";
-const FILES=["./","./index.html","./manifest.webmanifest","./icons/app-192.png","./icons/app-512.png"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));});
+self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate',event=>event.waitUntil((async()=>{
+  await Promise.all((await caches.keys()).map(key=>caches.delete(key)));
+  await self.registration.unregister();
+  const clients=await self.clients.matchAll({type:'window'});
+  await Promise.all(clients.map(client=>client.navigate(client.url)));
+})()));
