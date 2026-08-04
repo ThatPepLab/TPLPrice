5f228159616d7621e494fc5010e02e1c6403cac9
ThatPepLab <me@thateyeplace.com>
2026-08-04 12:59:51 -0500

    Fix vendor results and refresh offline cache
    Restore the missing China/USA region helper while keeping the Retail PDF feature removed. Advance the PWA cache to v2.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="peptide-price-comparison-august-v1";
+const CACHE="peptide-price-comparison-august-v2";
 const FILES=["./","./index.html","./manifest.webmanifest","./icons/app-192.png","./icons/app-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
